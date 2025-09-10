2d8b89b66fd5b43316899436345c914d7e42f9a8
LEO <leo@websoft.com.tw>
2025-09-10 10:58:39 +0800

    feat 加入 報修項目報價維護-匯入 報修項目報價維護-匯出

--- FET/FTT_API/Controllers/QuoteMgt/QuoteMgtController.cs
+++ FET/FTT_API/Controllers/QuoteMgt/QuoteMgtController.cs
@@ -22,12 +22,42 @@ namespace FTT_API.Controllers.QuoteMgt
         }
 
         [HttpPost("[action]")]
-        public IActionResult Import()
+        public IActionResult Import(IFormFile file)
         {
             try
             {
+                if (file == null || file.Length == 0)
+                    return Json(new { success = false, message = "未選擇檔案" });
 
-                return JsonSuccess("");
+                string ext = Path.GetExtension(file.FileName).ToLower();
+                if (ext != ".xls" && ext != ".xlsx")
+                    return Json(new { success = false, message = "檔案格式錯誤，只能上傳 Excel (.xls / .xlsx)" });
+
+                string AttachFileName = LoginSession.Current.empno + "_" + DateTime.Now.ToString("HHmmss") + "_" + file.FileName;
+                AttachFileName = System.IO.Path.GetFileName(_config.Config["OutputPath"] + AttachFileName);
+                string destFilePath = _config.Config["OutputPath"] + AttachFileName;
+
+                // 檢查資料夾是否存在
+                if (!Directory.Exists(_config.Config["OutputPath"]))
+                {
+                    Directory.CreateDirectory(_config.Config["OutputPath"]);
+                }
+
+                // 儲存檔案
+                using (var stream = new FileStream(destFilePath, FileMode.Create))
+                {
+                    file.CopyToAsync(stream);
+                }
+
+                QuoteMgtHanlder _QuoteMgtHanlder = new QuoteMgtHanlder(_config, HttpContext);
+                var msg = _QuoteMgtHanlder.Import(destFilePath);
+
+                if (string.IsNullOrEmpty(msg) == false)
+                {
+                    return JsonValidFail(msg);
+                }
+                else
+                    return JsonSuccess("匯入成功");
             }
             catch (Exception ex)
             {
@@ -40,13 +70,104 @@ namespace FTT_API.Controllers.QuoteMgt
         {
             try
             {
+                string fileName = "export_" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".xls";
 
-                return JsonSuccess("");
+                IWorkbook wb = new HSSFWorkbook();
+                ISheet ws;
+                ws = wb.CreateSheet("Sheet1");
+
+                DataTable dt = GetQueryData();
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    ws.CreateRow(0);
+                    //第一行為欄位名稱
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        ws.GetRow(0).CreateCell(i).SetCellValue(dt.Columns[i].ColumnName);
+                    }
+
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        ws.CreateRow(i + 1);
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                        {
+                            ws.GetRow(i + 1).CreateCell(j).SetCellValue(dt.Rows[i][j].ToString());
+                        }
+                    }
+                }
+                else
+                {
+                    ws.CreateRow(0);
+
+                    ws.GetRow(0).CreateCell(0).SetCellValue("更新註記");
+                    ws.GetRow(0).CreateCell(1).SetCellValue("ID");
+                    ws.GetRow(0).CreateCell(2).SetCellValue("CISID");
+                    ws.GetRow(0).CreateCell(3).SetCellValue("報修類別名稱");
+                    ws.GetRow(0).CreateCell(4).SetCellValue("費用種類");
+                    ws.GetRow(0).CreateCell(5).SetCellValue("維修細項_L1");
+                    ws.GetRow(0).CreateCell(6).SetCellValue("維修細項_L2");
+                    ws.GetRow(0).CreateCell(7).SetCellValue("維修細項_L3");
+                    ws.GetRow(0).CreateCell(8).SetCellValue("單位");
+                    ws.GetRow(0).CreateCell(9).SetCellValue("數量");
+                    ws.GetRow(0).CreateCell(10).SetCellValue("單價");
+                    ws.GetRow(0).CreateCell(11).SetCellValue("備註");
+                }
+
+                dt = GetCategoryData();
+                ws = wb.CreateSheet("報修類別");
+
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    ws.CreateRow(0);
+                    //第一行為欄位名稱
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        ws.GetRow(0).CreateCell(i).SetCellValue(dt.Columns[i].ColumnName);
+                    }
+
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        ws.CreateRow(i + 1);
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                        {
+                            ws.GetRow(i + 1).CreateCell(j).SetCellValue(dt.Rows[i][j].ToString());
+                        }
+                    }
+                }
+                else
+                {
+                }
+
+                byte[] fileContents;
+                // 輸出到 MemoryStream
+                using (var stream = new MemoryStream())
+                {
+                    wb.Write(stream, true);
+                    fileContents = stream.ToArray();
+                }
+
+                return File(fileContents,
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        fileName);
             }
             catch (Exception ex)
             {
                 return JsonValidFail("系統錯誤");
             }
+        }
+
+        private DataTable? GetCategoryData()
+        {
+            QuoteMgtHanlder _QuoteMgtHanlder = new QuoteMgtHanlder(_config, HttpContext);
+            DataTable dtTable = _QuoteMgtHanlder.GetCategoryData();
+            return dtTable;
+        }
+
+        private DataTable GetQueryData()
+        {
+            QuoteMgtHanlder _QuoteMgtHanlder = new QuoteMgtHanlder(_config, HttpContext);
+            DataTable dtTable = _QuoteMgtHanlder.GetQueryData();
+            return dtTable;
         }
 
         [HttpPost("[action]")]
@@ -78,9 +199,14 @@ namespace FTT_API.Controllers.QuoteMgt
                 }
 
                 QuoteMgtHanlder _QuoteMgtHanlder = new QuoteMgtHanlder(_config, HttpContext);
-                _QuoteMgtHanlder.ImportStore(destFilePath);
+                var msg = _QuoteMgtHanlder.ImportStore(destFilePath);
 
-                return JsonSuccess("匯入成功");
+                if (string.IsNullOrEmpty(msg) == false)
+                {
+                    return JsonValidFail(msg);
+                }
+                else
+                    return JsonSuccess("匯入成功");
             }
             catch (Exception ex)
             {
--- FET/FTT_API/Models/Handler/QuoteMgtHanlder.cs
+++ FET/FTT_API/Models/Handler/QuoteMgtHanlder.cs
@@ -40,6 +40,194 @@ namespace FTT_API.Models.Handler
             return dataTable;
         }
 
+        internal System.Data.DataTable GetCategoryData()
+        {
+            string strSql = "SELECT CISID AS CISID, CINAME AS 報修類別名稱 FROM CI_RELATIONS WHERE PARENTSID=1006 AND DISABLE IS NULL ORDER BY CISID";
+
+            var dataTable = dbHelper.FindDataTable(strSql, null);
+
+            return dataTable;
+        }
+
+        internal System.Data.DataTable GetQueryData()
+        {
+            string strSql = @"SELECT '' AS 更新註記,ID AS ID, CATEGORY_ID AS CISID, CATEGORY_NAME 報修類別名稱, EXPENSE_TYPE AS 費用種類, L1_DESC AS 維修細項_L1, L2_DESC 維修細項_L2, L3_DESC 維修細項_L3, UNIT AS 單位, QTY AS 數量, PRICE AS 單價, REMARK AS 備註 FROM AMOUNT_SELECT WHERE ENABLE='Y' ORDER BY CATEGORY_ID, ID";
+
+            var dataTable = dbHelper.FindDataTable(strSql, null);
+
+            return dataTable;
+        }
+
+        internal string? Import(string destFilePath)
+        {
+            IWorkbook wk;
+
+            string ext = Path.GetExtension(destFilePath).ToLower();
+
+            // 判斷副檔名
+            using (FileStream fs = new FileStream(destFilePath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                if (ext == ".xlsx")
+                {
+                    wk = new XSSFWorkbook(fs); // XSSF 讀取 .xlsx
+                }
+                else if (ext == ".xls")
+                {
+                    wk = new HSSFWorkbook(fs); // HSSF 讀取 .xls
+                }
+                else
+                {
+                    return "檔案格式錯誤，只能上傳 Excel (.xls / .xlsx)";
+                }
+            }
+
+            for (int k = 0; k < wk.NumberOfSheets; k++)
+            {
+                ISheet hst = wk.GetSheetAt(k);
+
+                if (hst.SheetName == "Sheet1")
+                {
+                    IRow hr = hst.GetRow(0);
+
+                    for (int j = 1; j <= hst.LastRowNum; j++)
+                    {
+                        int line = j + 1;
+
+                        hr = hst.GetRow(j);
+
+                        string strFlag = hr.GetCell(0) == null ? "0" : hr.GetCell(0).ToString().Trim();
+                        string strID = hr.GetCell(1) == null ? "0" : hr.GetCell(1).ToString().Trim();
+                        string strCisid = hr.GetCell(2) == null ? "" : hr.GetCell(2).ToString().Trim();
+                        string strExpenseType = hr.GetCell(4) == null ? "" : hr.GetCell(4).ToString().Trim();
+                        string strL1Desc = hr.GetCell(5) == null ? "" : hr.GetCell(5).ToString().Trim();
+                        string strL2Desc = hr.GetCell(6) == null ? "" : hr.GetCell(6).ToString().Trim();
+                        string strL3Desc = hr.GetCell(7) == null ? "" : hr.GetCell(7).ToString().Trim();
+                        string strUnit = hr.GetCell(8) == null ? "" : hr.GetCell(8).ToString().Trim();
+                        string strQty = hr.GetCell(9) == null ? "null" : hr.GetCell(9).ToString().Trim();
+                        string strPrice = hr.GetCell(10) == null ? "null" : hr.GetCell(10).ToString().Trim();
+                        string strRemark = hr.GetCell(11) == null ? "" : hr.GetCell(11).ToString().Trim();
+
+                        var intCisid = 0;
+
+                        if (int.TryParse(strCisid, out intCisid) == false)
+                        {
+                            return $"第 {line.ToString()} 列 CISID 應為整數!";
+                        }
+
+                        double intQty = 0;
+
+                        if (double.TryParse(strQty, out intQty) == false)
+                        {
+                            return $"第 {line.ToString()} 列 數量 應為數值!";
+                        }
+
+                        int intPrice = 0;
+
+                        if (int.TryParse(strPrice, out intPrice) == false)
+                        {
+                            return $"第 {line.ToString()} 列 單價 應為整數!";
+                        }
+
+                        var paras = new Dictionary<string, object>();
+
+                        if (strFlag == "D")
+                        {
+                            paras = new Dictionary<string, object>();
+                            paras.Add("strID", strID);
+                            paras.Add("MODIFY_OPERATOR", LoginSession.Current.empno);
+
+                            //string deleteSql = string.Format("UPDATE AMOUNT_SELECT SET ENABLE='N', UPDATE_TIME=SYSDATE, MODIFY_OPERATOR='{1}' WHERE ENABLE='Y' AND ID={0}", strID, Context.User.Identity.Name);
+                            string deleteSql = "UPDATE AMOUNT_SELECT SET ENABLE='N', UPDATE_TIME=SYSDATE, MODIFY_OPERATOR=@MODIFY_OPERATOR WHERE ENABLE='Y' AND ID=@strID ";
+                            this.dbHelper.Execute(deleteSql, paras);
+                            this.dbHelper.Commit();
+                        }
+                        else if (strFlag == "A")
+                        {
+                            paras = new Dictionary<string, object>();
+                            paras.Add("strCisid", intCisid);
+                            paras.Add("strExpenseType", strExpenseType.Replace("'", "''"));
+                            paras.Add("strL1Desc", strL1Desc.Replace("'", "''"));
+                            paras.Add("strL2Desc", strL2Desc.Replace("'", "''"));
+                            paras.Add("strL3Desc", strL3Desc.Replace("'", "''"));
+                            paras.Add("strUnit", strUnit);
+                            paras.Add("strQty", intQty);
+                            paras.Add("strPrice", intPrice);
+                            paras.Add("strRemark", strRemark.Replace("'", "''"));
+                            paras.Add("MODIFY_OPERATOR", LoginSession.Current.empno);
+                            //string insertSql = string.Format("INSERT INTO AMOUNT_SELECT (CATEGORY_ID, EXPENSE_TYPE, L1_DESC, L2_DESC, L3_DESC, UNIT, QTY, PRICE, REMARK, MODIFY_OPERATOR) VALUES ({0},'{1}','{2}','{3}','{4}','{5}',{6},{7},'{8}','{9}')", strCisid, strExpenseType.Replace("'", "''"), strL1Desc.Replace("'", "''"), strL2Desc.Replace("'", "''"), strL3Desc.Replace("'", "''"), strUnit, strQty, strPrice, strRemark.Replace("'", "''"), Context.User.Identity.Name);
+                            string insertSql = @"
+INSERT INTO AMOUNT_SELECT 
+(CATEGORY_ID, EXPENSE_TYPE, L1_DESC, L2_DESC, L3_DESC, UNIT, QTY, PRICE, REMARK, MODIFY_OPERATOR) 
+VALUES 
+(@strCisid,@strExpenseType,@strL1Desc,@strL2Desc,@strL3Desc,@strUnit,@strQty,@strPrice,@strRemark,@MODIFY_OPERATOR) ";
+
+                            this.dbHelper.Execute(insertSql, paras);
+                            this.dbHelper.Commit();
+                        }
+                        else
+                        {
+                            if (strCisid != "0")
+                            {
+                                if (CheckDataExist("CI_RELATIONS", " CISID=@CISID AND PARENTSID=1006 AND DISABLE IS NULL ", new Dictionary<string, object>() { { "CISID", intCisid } }) == false)
+                                {
+                                    return $"第 {line.ToString()} 列 無此({strCisid}) CISID!";
+                                    //SystemModelClass.RegisterClientScript("Finished", "alert('第 " + line.ToString() + " 列 無此(" + strCisid + ") CISID!');");
+                                    //hasErr = true;
+                                }
+                                else
+                                {
+                                    if (CheckDataExist("AMOUNT_SELECT", "ID=@strID AND ENABLE='Y'", new Dictionary<string, object>() { { "strID", strID } }) == false)
+                                    {
+                                        return $"第 {line.ToString()} 列 無此({strID}) ID!";
+                                        //SystemModelClass.RegisterClientScript("Finished", "alert('第 " + line.ToString() + " 列 無此(" + strID + ") ID!');");
+                                        //hasErr = true;
+                                    }
+                                    else
+                                    {
+                                        paras = new Dictionary<string, object>();
+                                        paras.Add("strID", strID);
+                                        paras.Add("strCisid", intCisid);
+                                        paras.Add("strExpenseType", strExpenseType.Replace("'", "''"));
+                                        paras.Add("strL1Desc", strL1Desc.Replace("'", "''"));
+                                        paras.Add("strL2Desc", strL2Desc.Replace("'", "''"));
+                                        paras.Add("strL3Desc", strL3Desc.Replace("'", "''"));
+                                        paras.Add("strUnit", strUnit);
+                                        paras.Add("strQty", intQty);
+                                        paras.Add("strPrice", intPrice);
+                                        paras.Add("strRemark", strRemark.Replace("'", "''"));
+                                        paras.Add("MODIFY_OPERATOR", LoginSession.Current.empno);
+
+                                        //string updateSql = string.Format("UPDATE AMOUNT_SELECT SET CATEGORY_ID='{1}', EXPENSE_TYPE='{2}', L1_DESC='{3}', L2_DESC='{4}', L3_DESC='{5}', UNIT='{6}', QTY={7}, PRICE={8}, REMARK='{9}', MODIFY_OPERATOR='{10}' WHERE ID={0} AND ENABLE='Y'", strID, strCisid, strExpenseType.Replace("'", "''"), strL1Desc.Replace("'", "''"), strL2Desc.Replace("'", "''"), strL3Desc.Replace("'", "''"), strUnit, strQty, strPrice, strRemark.Replace("'", "''"), Context.User.Identity.Name);
+                                        string updateSql = @"
+UPDATE AMOUNT_SELECT 
+SET 
+CATEGORY_ID=@strCisid, 
+EXPENSE_TYPE=@strExpenseType, 
+L1_DESC=@strL1Desc, 
+L2_DESC=@strL2Desc, 
+L3_DESC=@strL3Desc, 
+UNIT=@strUnit, 
+QTY=@strQty, 
+PRICE=@strPrice, 
+REMARK=@strRemark, 
+MODIFY_OPERATOR=@MODIFY_OPERATOR 
+WHERE ID=@strID
+AND ENABLE='Y'";
+
+                                        this.dbHelper.Execute(updateSql, paras);
+                                        this.dbHelper.Commit();
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            return "";
+        }
+
         internal string ImportStore(string destFilePath)
         {
             IWorkbook wk;
@@ -70,7 +258,6 @@ namespace FTT_API.Models.Handler
                 if (hst.SheetName == "報修類別設定")
                 {
                     IRow hr = hst.GetRow(0);
-                    int dLastNum = hr.LastCellNum;
 
                     for (int j = 1; j <= hst.LastRowNum; j++)
                     {
@@ -80,23 +267,30 @@ namespace FTT_API.Models.Handler
                         string strCisid = hr.GetCell(0)?.ToString().Trim() ?? "";
                         string strSelfConfig = hr.GetCell(7)?.ToString().Trim() ?? "";
 
+                        var intCisid = 0;
+
+                        if (int.TryParse(strCisid, out intCisid) == false)
+                        {
+                            return $"第 {line.ToString()} 列 CISID應為整數!";
+                        }
+
                         if (strCisid != "0")
                         {
-                            if (!CheckDataExist("CI_RELATIONS", new Dictionary<string, object>() { { "CISID", strCisid } }))
+                            if (!CheckDataExist("CI_RELATIONS", new Dictionary<string, object>() { { "CISID", intCisid } }))
                             {
                                 return $"第 {line} 列 無此({strCisid}) CISID!";
                             }
                             else
                             {
                                 var paras = new Dictionary<string, object>
-                        {
-                            { "strCisid", strCisid },
-                            { "strSelfConfig", strSelfConfig }
-                        };
+                                {
+                                    { "strCisid", intCisid },
+                                    { "strSelfConfig", strSelfConfig }
+                                };
 
                                 string updateSql;
 
-                                if (CheckDataExist("CI_RELATIONS_CATEGORY", new Dictionary<string, object>() { { "CISID", strCisid } }))
+                                if (CheckDataExist("CI_RELATIONS_CATEGORY", new Dictionary<string, object>() { { "CISID", intCisid } }))
                                 {
                                     updateSql = "UPDATE CI_RELATIONS_CATEGORY SET SELFCONFIG=@strSelfConfig WHERE CISID=@strCisid";
                                 }
@@ -115,77 +309,6 @@ namespace FTT_API.Models.Handler
 
             return "";
         }
-
-        internal string ImportStore2(string destFilePath)
-        {
-            string strFilePath = string.Format(destFilePath);
-
-            XSSFWorkbook wk;
-            XSSFSheet hst;
-            XSSFRow hr;
-
-            using (FileStream fs = new FileStream(strFilePath, FileMode.Open, FileAccess.ReadWrite))
-            {
-                wk = new XSSFWorkbook(fs);
-            }
-
-            for (int k = 0; k < wk.Count; k++)
-            {
-                hst = (XSSFSheet)wk.GetSheetAt(k);
-
-                if (hst.SheetName == "報修類別設定")
-                {
-                    hr = (XSSFRow)hst.GetRow(0);
-                    int dLastNum = hr.LastCellNum;
-
-                    bool hasErr = false;
-
-                    for (int j = 1; j <= hst.LastRowNum; j++)
-                    {
-                        int line = j + 1;
-
-                        hr = (XSSFRow)hst.GetRow(j);
-
-                        string strCisid = hr.GetCell(0) == null ? "" : hr.GetCell(0).ToString().Trim();
-                        string strSelfConfig = hr.GetCell(7) == null ? "" : hr.GetCell(7).ToString().Trim();
-
-                        if (strCisid != "0")
-                        {
-                            //if (db.CheckDataExist("CI_RELATIONS", "CISID='" + strCisid + "'") == false)
-                            if (CheckDataExist("CI_RELATIONS", new Dictionary<string, object>() { { "CISID", strCisid } }) == false)
-                            {
-                                //SystemModelClass.RegisterClientScript("Finished", "alert('第 " + line.ToString() + " 列 無此(" + strCisid + ") CISID!');");
-                                return $"第 {line} 列 無此({strCisid}) CISID!');";
-                            }
-                            else
-                            {
-                                var paras = new Dictionary<string, object>();
-                                paras.Add("strCisid", strCisid);
-                                paras.Add("strSelfConfig", strSelfConfig);
-
-                                string updateSql = "";
-
-                                //if (db.CheckDataExist("CI_RELATIONS_CATEGORY", "CISID='" + strCisid + "'") == true)
-                                if (CheckDataExist("CI_RELATIONS_CATEGORY", new Dictionary<string, object>() { { "CISID", strCisid } }) == true)
-                                {
-                                    updateSql = string.Format(@"UPDATE CI_RELATIONS_CATEGORY SET SELFCONFIG=@strSelfConfig WHERE CISID=@strCisid ", strCisid, strSelfConfig);
-                                }
-                                else
-                                {
-                                    updateSql = string.Format(@"INSERT INTO CI_RELATIONS_CATEGORY (CISID,SELFCONFIG) VALUES (@strCisid,@strSelfConfig)", strCisid, strSelfConfig);
-                                }
-
-                                dbHelper.Execute(updateSql, paras);
-                                dbHelper.Commit();
-                            }
-                        }
-                    }
-                }
-            }
-
-            return "";
-        }
-
 
         internal void SaveMarquee(string content)
         {
